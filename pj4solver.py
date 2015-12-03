ed79a0183405e717c9401d4711e659a5359dd837
jholkeboer <jholkeboer@users.noreply.github.com>
2015-12-02 20:25:52 -0800

    tour output heuristic

--- pj4solver.py
+++ pj4solver.py
@@ -18,17 +18,15 @@ def node_distance(node1, node2):
 	distance = int(round(sqrt( (node1[0]-node2[0])**2 + (node1[1]-node2[1])**2 )))
 	return distance
 
-
-
 def import_problem(inputfile):
-	# nodes = {}
+	node_dict = {}
 	nodes = []
 	with open(inputfile) as f:
 		for line in f:
 			parsed_line = line.strip().split(" ")
-			# nodes[int(parsed_line[0])] = [int(parsed_line[1]), int(parsed_line[2])]
+			node_dict[parsed_line[1] + "-" + parsed_line[2]] = int(parsed_line[0])
 			nodes.append([int(parsed_line[1]), int(parsed_line[2])])
-	return nodes
+	return nodes, node_dict
 
 def export_solution(tour, outputfile):
 	with open(outputfile, 'w+') as f:
@@ -36,55 +34,41 @@ def export_solution(tour, outputfile):
 		for node in tour['path']:
 			f.write(str(node) + "\n")
 
-def tsp(points):
-
-    start = points[0]
-    must_visit = points
-    path = [start]
-    must_visit.remove(start)
-    while must_visit:
-        nearest = min(must_visit, key=lambda x: node_distance(path[-1], x))
-        path.append(nearest)
-        must_visit.remove(nearest)
-
-    #print path
+def tsp(points, node_dict):
+	start = points[0]
+	must_visit = points
+	path = [start]
+	must_visit.remove(start)
+	while must_visit:
+		nearest = min(must_visit, key=lambda x: node_distance(path[-1], x))
+		path.append(nearest)
+		must_visit.remove(nearest)
 	
+	tour = {'length': 0, 'path': []}
 	
-	# all_distances = [[node_distance(nodes[x], nodes[y]) for y in sorted(nodes)] for x in sorted(nodes)]
-	# A = {(frozenset([0, idx+1]), idx+1): (dist, [0,idx+1]) for idx,dist in enumerate(all_distances[0][1:])}
+	# get node indices
+	for n in path:
+		node_key = str(n[0]) + "-" + str(n[1])
+		tour['path'].append(node_dict[node_key])
 	
-	# for node in range(2, node_count):
-	# 	print "Node ", node
-	# 	B = {}
-	# 	for S in [frozenset(C) | {0} for C in itertools.combinations(range(1, node_count), node)]:
-	# 		for j in S - {0}:
-	# 			B[(S, j)] = min( [(A[(S-{j},k)][0] + all_distances[k][j], A[(S-{j},k)][1] + [j]) for k in S if k != 0 and k!=j])
-	# 	A = B
+	# calculate path length
+	for i in range(1, len(path)):
+		tour['length'] += node_distance(path[i-1],path[i])
 
-	# res = min([(A[d][0] + all_distances[0][d[1]], A[d][1]) for d in iter(A)])
-	# tour['path'] = res[1]
-	# tour['length'] = res[0]
-    return path
+	return tour
 
 
 if len(sys.argv) == 2:
 	inputfile = sys.argv[1]
 	outputfile = inputfile + ".tour"
-	nodes = import_problem(inputfile)
+	nodes, node_dict = import_problem(inputfile)
 
 	start = datetime.datetime.now()
-	tour = tsp(nodes)
+	tour = tsp(nodes, node_dict)
 	end = datetime.datetime.now()
 	print tour
 	print "Time elapsed: ", (end - start)
 	# export_solution(tour, outputfile)
 	
-	
 else:
 	print "Usage: pj4solver.py [inputfile]"
-	
-def print_sol(sol):
-    num = 0
-    for x in range(0,len(sol)):
-        print(str(x)+ '  ' + str(sol[x][0]) + '  ' + str(sol[x][1]))
-        x += 1
